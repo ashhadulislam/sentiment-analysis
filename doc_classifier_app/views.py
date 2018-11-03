207ec0a15573f035e11c75a8d877250f93b3f39c
ashhadulislam@gmail.com <ashhadulislam@gmail.com>
2018-11-04 00:35:00 +0530

    3rd  commit

--- doc_classifier_app/views.py
+++ doc_classifier_app/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+from django.template import loader
+
+
+from . import constants
+
+import os
+
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the doc_classifier app index.")
+    print(constants.output_results_location)
+    print(os.listdir(constants.output_results_location))
+
+    context=constants.update_context()
+    template = loader.get_template('doc_classifier_app/index.html')
+    return HttpResponse(template.render(context, request))
+    # return HttpResponse("Hello, world. You're at the doc_classifier app index.")
